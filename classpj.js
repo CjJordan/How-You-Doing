d4a07d268c467335b0142bb29bab31037352e919
Lauriezip <lauriezip@gmail.com>
2017-03-18 10:58:38 -0400

    Add files via upload
    Adding missing code

--- classpj.js
+++ classpj.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
 
 
-<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
-  <script type="text/javascript>
-    // The below code fills in the first row of the table
-    var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v2?text=Happy&apikey=4f46adf1-10d7-4023-ba43-7bd79cec1e59";
+
+    var textInput = "If I had a pony, I get on it and ride away from here as fast as I could."
+
+    var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v2?text="+textInput+"&apikey=4f46adf1-10d7-4023-ba43-7bd79cec1e59";
+
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
-    	console.log (response);
+	    
+	    var score = (response.sentiment_analysis[0].aggregate.score);
+	    console.log (score);
+
+	    console.log (response.sentiment_analysis[0].aggregate);
+
+	    
+	    var sentiment = (response.sentiment_analysis[0].aggregate.sentiment);
+
+      console.log (response.sentiment_analysis[0].aggregate.sentiment);
+
+
+});
+
+});
         
-        }
+		   
 
 
 
@@ -19,4 +34,3 @@ $(document).ready(function(){
 
 
 
-}); //end ready function
